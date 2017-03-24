def3d6a2777d0999bf746e142abd6cc174bb9528
Abhishek <chandani.abhishek440@gmail.com>
2017-03-25 03:10:40 +0530

    Updated the Example App UI.

--- ACFloatingTextfield-Objc/TFDemoApp/ViewController.m
+++ ACFloatingTextfield-Objc/TFDemoApp/ViewController.m
@@ -10,51 +10,43 @@
 #import "ACFloatingTextField.h"
 
 @interface ViewController ()<UITextFieldDelegate>
-{
-    ACFloatingTextField *tf_, *tf_2;
-}
-@property (weak, nonatomic) IBOutlet ACFloatingTextField *textField;
-@property (weak, nonatomic) IBOutlet UIStackView *containerForTexts;
+@property (weak, nonatomic) IBOutlet ACFloatingTextField *textFieldUsername;
+@property (weak, nonatomic) IBOutlet ACFloatingTextField *textFieldPassword;
 
 @end
 
 @implementation ViewController
 
 - (IBAction)showErrorTap:(UIButton *)sender {
-    [tf_ showError];
-    [tf_2 showErrorWithText:@"This is New Error"];
-    [_textField showError];
+    [_textFieldPassword showErrorWithText:@"This is Va Error"];
+    [_textFieldUsername showErrorWithText:@"Password should not less then 6 characters."];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [_textField setTextFieldPlaceholderText:@"Username"];
-    _textField.selectedLineColor = [UIColor orangeColor];
-    _textField.placeHolderColor = [UIColor redColor];
-    _textField.selectedPlaceHolderColor = [UIColor greenColor];
-    _textField.lineColor = [UIColor blueColor];
-
-    tf_ = [[ACFloatingTextField alloc] init];
-    [tf_ setTextFieldPlaceholderText:@"Placeholder"];
-    [tf_ setText:@"Abhishek"];
-    tf_.text = @"Chandani";
-    [self.containerForTexts addArrangedSubview:tf_];
+    _textFieldUsername.backgroundColor = [UIColor clearColor];
+    _textFieldPassword.backgroundColor = [UIColor clearColor];
+    _textFieldUsername.textColor = [UIColor whiteColor];
+    _textFieldPassword.textColor = [UIColor whiteColor];
     
-    
-    tf_2 = [[ACFloatingTextField alloc] init];
-    [tf_2 setTextFieldPlaceholderText:@"Placeholder"];
-    [tf_2 setText:@"Abhishek"];
+    _textFieldUsername.selectedLineColor = [UIColor yellowColor];
+    _textFieldUsername.selectedPlaceHolderColor = [UIColor yellowColor];
+    _textFieldUsername.placeHolderColor = [UIColor whiteColor];
+    _textFieldUsername.lineColor = [UIColor whiteColor];
 
-    [self.containerForTexts addArrangedSubview:tf_2];
-    tf_.errorText = @"You can not write do this!";
-    _textField.errorText = @"Haaa";
-    tf_2.errorText = @"No crejo";
+    _textFieldPassword.selectedLineColor = [UIColor yellowColor];
+    _textFieldPassword.selectedPlaceHolderColor = [UIColor yellowColor];
+    _textFieldPassword.placeHolderColor = [UIColor whiteColor];
+    _textFieldPassword.lineColor = [UIColor whiteColor];
 
 }
 
 #pragma mark  UITextfield Delegates
-
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return true;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
